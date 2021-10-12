46e774d929193870b543963c1fbf8d13be996b88
Infant Ajay <infantcsajay@outlook.com>
2021-10-13 05:28:59 +0530

    [server] add multer middleware for file handling

--- index.js
+++ index.js
@@ -1,12 +1,21 @@
-const app = require('express')();
+const app = require("express")();
+const upload = require("multer")({ dest: "./uploads" });
 
-app.set('views', './templates');
-app.set('view engine', 'pug');
+app.set("views", "./templates");
+app.set("view engine", "pug");
 
-app.get('/', (req, res) =>{
- res.render('index');
-})
+app.get("/", (_, res) => {
+  res.render("index");
+});
 
-app.listen(3000, ()=>{
-  console.log('listening on port 3000');
-})
+app.post("/submit", upload.single("uploaded_file"), (_, res) => {
+  try {
+    res.redirect('/');
+  } catch (error) {
+    console.error(error);
+  }
+});
+
+app.listen(3000, () => {
+  console.log("listening on port 3000");
+});

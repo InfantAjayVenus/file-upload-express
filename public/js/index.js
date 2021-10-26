6f1292a7548deac1febfe958bbc47223097d6e79
Infant Ajay <infantcsajay@outlook.com>
2021-10-26 09:21:49 +0530

    [UI] add upload status indicators

--- public/js/index.js
+++ public/js/index.js
@@ -1,14 +1,34 @@
+function getStatus(statusContent) {
+  const status = document.createElement("div");
+  status.setAttribute("id", "status");
+  status.innerHTML = statusContent;
+  return status;
+}
 
 function handleSubmit(e) {
   e.preventDefault();
-  const {files} = document.querySelector('#uploadForm>input[name="upload_file"]');
+  const { files } = document.querySelector(
+    '#uploadForm>input[name="upload_file"]'
+  );
+  const successStatus = `<p style="color: green">File uploaded Successfully</p>`;
+  const failedStatus = `<p style="color: red">Error Uploading File!</p>`;
   const formData = new FormData();
-  formData.append('file', files[0]);
+  formData.append("file", files[0]);
+  document.querySelector("#status") &&
+    document.body.removeChild(document.querySelector("#status"));
 
-  fetch('/submit', {
-    method: 'post',
+  fetch("/submit", {
+    method: "post",
     body: formData,
   })
+    .then((response) => {
+      if (response.ok && response.status === 200)
+        document.body.appendChild(getStatus(successStatus));
+    })
+    .catch((error) => {
+      document.body.appendChild(getStatus(failedStatus));
+      console.debug(error);
+    });
 }
 
-document.querySelector('#uploadForm').addEventListener('submit', handleSubmit)
+document.querySelector("#uploadForm").addEventListener("submit", handleSubmit);

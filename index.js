e685ed426554a9a8cc37fbcb6dac1150e3494823
kishore Jr <36214175+kishoreravi24@users.noreply.github.com>
2021-11-27 22:29:41 +0530

    new feature, validation added

--- index.js
+++ index.js
@@ -1,16 +1,31 @@
 const express = require("express");
+const multer = require("multer")
 const app = express();
-const upload = require("multer")({ dest: "./uploads" , preservePath: true});
+const path = require('path')
+global.crypto = require('crypto')
 
 app.use(express.static('public'));
 app.set("views", "./templates");
 app.set("view engine", "pug");
 
+const storage = multer.diskStorage({
+  destination: './uploads/',
+  filename: function (req, file, cb) {
+    crypto.pseudoRandomBytes(16, function (err, raw) {
+      if (err) return cb(err)
+
+      cb(null, raw.toString('hex') + path.extname(file.originalname))
+    })
+  }
+})
+
+const upload = multer({ storage: storage })
+
 app.get("/", (_, res) => {
   res.render("index");
 });
 
-app.post("/submit", upload.single("file"), (_, res) => {
+app.post("/submit", upload.array("file",10), (req, res) => {
   try {
     res.sendStatus(200);
   } catch (error) {
